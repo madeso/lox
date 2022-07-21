3e0804194e3f5e3c1ac864983e4d3353b47b4a88
Gustav <sir.gustav.the.coder@gmail.com>
2022-07-21 12:14:53 +0200

    style: more source cleanup

--- src/lox/token.cc
+++ src/lox/token.cc
@@ -3,16 +3,20 @@
 
 namespace lox
 {
-    Token::Token(TokenType the_type, const std::string& the_lexeme, std::shared_ptr<Object> the_literal, int the_line)
-        : type(the_type)
-        , lexeme(the_lexeme)
-        , literal(the_literal)
-        , line(the_line)
-    {
-    }
 
-    std::string Token::toString() const
-    {
-        return fmt::format("{0} {1} {2}", ToString(type), lexeme, literal != nullptr ? literal->toString() : "");
-    }
+    
+Token::Token(TokenType the_type, const std::string& the_lexeme, std::shared_ptr<Object> the_literal, int the_line)
+    : type(the_type)
+    , lexeme(the_lexeme)
+    , literal(the_literal)
+    , line(the_line)
+{
+}
+
+std::string Token::toString() const
+{
+    return fmt::format("{0} {1} {2}", ToString(type), lexeme, literal != nullptr ? literal->toString() : "");
+}
+
+
 }
